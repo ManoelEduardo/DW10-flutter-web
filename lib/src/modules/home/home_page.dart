afbc2c4586462f1f7fd26afa76ff523b372410f4
Manoel Eduardo <manoeleduardo.sistemas@gmail.com>
2023-05-09 20:05:51 -0300

    Second day of DW10

--- lib/src/modules/home/home_page.dart
+++ lib/src/modules/home/home_page.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-
-import '../../core/env/env.dart';
 import '../../core/ui/helpers/loader.dart';
 import '../../core/ui/helpers/messages.dart';
 
@@ -14,22 +12,21 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> with Loader, Messages {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home Page'),
-      ),
-      body: Text(
-        Env.instance.get('backend_base_url'),
-      ),
-      bottomNavigationBar: FloatingActionButton(
-        onPressed: () async {
-          showLoader();
-          await Future.delayed(const Duration(seconds: 2));
-          hideLoader();
-
-          showError('Erro de alguma coisa');
-        },
-        child: const Icon(Icons.abc),
+    return SizedBox(
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Form(
+              autovalidateMode: AutovalidateMode.onUserInteraction,
+              child: TextFormField(
+                decoration: const InputDecoration(
+                  label: Text('Login'),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
