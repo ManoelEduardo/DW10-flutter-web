9f7ea9ee01a9d6a4bd0882bbaa69da59463cf58d
Manoel Eduardo <manoeleduardo.sistemas@gmail.com>
2023-05-08 21:59:59 -0300

    Implementing login_service

--- lib/src/models/auth_repository.dart
+++ lib/src/models/auth_repository.dart
@@ -1,21 +1,21 @@
 import 'dart:convert';
 
 class AuthModel {
-  final String acessToken;
+  final String accessToken;
 
   AuthModel({
-    required this.acessToken,
+    required this.accessToken,
   });
 
   Map<String, dynamic> toMap() {
     return <String, dynamic>{
-      'acess_token': acessToken,
+      'access_token': accessToken,
     };
   }
 
   factory AuthModel.fromMap(Map<String, dynamic> map) {
     return AuthModel(
-      acessToken: (map['acess_token'] ?? '') as String,
+      accessToken: (map['access_token'] ?? '') as String,
     );
   }
 
