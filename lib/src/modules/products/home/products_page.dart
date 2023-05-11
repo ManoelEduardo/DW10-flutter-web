e7aa81b21740370ce4aa3a65178a753717d6bc99
Manoel Eduardo <manoeleduardo.sistemas@gmail.com>
2023-05-11 01:46:15 -0300

    Adjusting

--- lib/src/modules/products/home/products_page.dart
+++ lib/src/modules/products/home/products_page.dart
@@ -1,10 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:mobx/mobx.dart';
 
+import '../../../core/ui/helpers/loader.dart';
+import '../../../core/ui/helpers/messages.dart';
 import '../../../core/ui/widgets/base_header.dart';
+import 'products_controller.dart';
 import 'widgets/product_item.dart';
 
-class ProductsPage extends StatelessWidget {
+class ProductsPage extends StatefulWidget {
   const ProductsPage({Key? key}) : super(key: key);
+
+  @override
+  State<ProductsPage> createState() => _ProductsPageState();
+}
+
+class _ProductsPageState extends State<ProductsPage> with Loader, Messages {
+  final producstController = Modular.get<ProductsController>();
+  late final ReactionDisposer statusDisposer;
+
+  @override
+  void initState() {
+    WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+      statusDisposer = reaction((_) => producstController.status, (status) {
+        switch (status) {
+          case ProductStateStatus.initial:
+            break;
+          case ProductStateStatus.loading:
+            showLoader();
+            break;
+          case ProductStateStatus.loaded:
+            hideLoader();
+            break;
+          case ProductStateStatus.error:
+            hideLoader();
+            showError('Erro ao buscar produtos');
+            break;
+        }
+      });
+      producstController.loadProducts();
+    });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    statusDisposer();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
